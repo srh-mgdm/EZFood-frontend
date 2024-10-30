558d41e455c6c7ea1ec391cc3cde6bd38ede582f
EverAsunder <ever.asunder@gmail.com>
2024-10-30 14:52:38 +0100

    modifs style card

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Image,
   ImageBackground,
@@ -9,59 +9,42 @@ import {
   View,
   Button,
   ScrollView,
-} from 'react-native';
-import {Header} from '../components/Header'
-import {Card} from '../components/Card'
-import { NavigationProp, ParamListBase } from '@react-navigation/native';
+} from "react-native";
+import { Header } from "../components/Header";
+import { Card } from "../components/Card";
+import { NavigationProp, ParamListBase } from "@react-navigation/native";
 
 export default function HomeScreen({ navigation }) {
-
-
-
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.head}>
-                <Header navigation={navigation}/>
-            </View>
-            <ScrollView contentContainerStyle={styles.main}>
-
-                <Card navigation={navigation}/>
-                
-                
-        </ScrollView>
-        </View>
-
-    )
-};
-
+  return (
+    <View style={styles.container}>
+      <View style={styles.head}>
+        <Header navigation={navigation} />
+      </View>
+      <ScrollView contentContainerStyle={styles.main}>
+        <Card navigation={navigation} />
+      </ScrollView>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        marginTop: '10%',
-
-    },
-
-    head: {
-        height: 50,
-        width: '100%'
-    },
-
-    main: {
-
-    },
-
-    main: {
-        flexGrow: 5,
-        paddingBottom: 10,
-        display: "flex",
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        flexWrap: 'wrap',
-        minHeight: "100%",
-        maxHeight: '500%',
-        paddingBottom: 10,
-    }
-})
-
+  container: {
+    flex: 1,
+    marginTop: "10%",
+  },
+  head: {
+    height: 50,
+    width: "100%",
+  },
+  main: {
+    flexGrow: 5,
+    paddingBottom: 10,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    flexWrap: "wrap",
+    minHeight: "100%",
+    maxHeight: "500%",
+    paddingBottom: 10,
+  },
+});
