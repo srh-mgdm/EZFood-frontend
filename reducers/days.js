6ab85bea656c63ec97b255483d0b9032f8263d31
EverAsunder <ever.asunder@gmail.com>
2024-10-31 18:09:01 +0100

    days full update with remove, still a rerender issue

--- reducers/days.js
+++ reducers/days.js
@@ -17,12 +17,26 @@ export const daysSlice = createSlice({
     deleteDay: (state, action) => {
       state.value = action.payload;
     },
-    deleteMeal: (state, action) => {
-      state.value = action.payload;
+    deleteMealFromDay: (state, action) => {
+      //   const { dayId, mealPosition } = action.payload;
+      //   const day = state.value.find((day) => day.dayId === dayId);
+      //   if (day) {
+      //     day.meals[mealPosition] = { mealId: null, mealName: null };
+      //   }
+
+      const { dayId, mealPosition } = action.payload;
+      const updatedDay = state.value.find((day) => day.dayId === dayId);
+      if (!updatedDay) {
+        return;
+      }
+      const updatedMeals = [...updatedDay.meals];
+      updatedMeals[mealPosition] = { mealId: null, mealName: null };
+      return { ...day, meals: updatedMeals };
     },
   },
 });
 
-export const { addDay, setDays, deleteDay, deleteMeal } = daysSlice.actions;
+export const { addDay, setDays, deleteDay, deleteMealFromDay } =
+  daysSlice.actions;
 
 export default daysSlice.reducer;
